85f77c8e901f0f698cd316e42d57755dc3ad2032
Bent Hartz <Bent Hartz>
2017-07-19 21:36:47 +0700

    Bug fix: Constant speed on different device resolution smooth pan Zoom out motion zoom pan on image touch only

--- pixelmanipulated.cs
+++ pixelmanipulated.cs
@@ -34,10 +34,33 @@ public class pixelmanipulated : MonoBehaviour {
 
 	//zoom and pan
 
+	//canvas scaler to fix diffferent speed in different screen issue
+	private CanvasScaler canvasScaler;
+	private Vector2 ScreenScale
+	{
+		get
+		{
+			if (canvasScaler == null)
+			{
+				canvasScaler = GetComponentInParent<CanvasScaler>();
+			}
+
+			if (canvasScaler)
+			{
+				return new Vector2(canvasScaler.referenceResolution.x / Screen.width, canvasScaler.referenceResolution.y / Screen.height);
+			}
+			else
+			{
+				return Vector2.one;
+			}
+		}
+	}
+
 	//speed
-	public float zoomSpeed = 0.001f;
-	public float panSpeed = 0.05f;
-	public float backToCenterSpeed = 0.3f;
+	public float zoomSpeed = 5f;
+	public float panSpeed = 250f;
+
+	public float nudgeRange=0.5f;
 
 	private RectTransform imageTransform;
 	private float scaling = 1.0f;
@@ -60,9 +83,15 @@ public class pixelmanipulated : MonoBehaviour {
 	public float holdRate = 0.1f;
 	private float nextWait = 0.0f;
 
+	//specific touch
+	private bool isTouching = false;
+
 
 	// Use this for initialization
 	void Start () {
+
+		isTouching = false;
+
 
 		//fill
 
@@ -128,100 +157,137 @@ public class pixelmanipulated : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-
-		if (Input.touchCount == 2)      //For Detecting Multiple Touch On Screen
-		{
-			if (Input.GetTouch(0).phase == TouchPhase.Began) {
-				//set fill delay parameter
-				nextWait = Time.time + holdRate;
-			}
+		if (isTouching) {
 
 
-			// Store both touches.
-			Touch touchZero = Input.GetTouch(0);
-			Touch touchOne = Input.GetTouch(1);
-
-			// Find the position in the previous frame of each touch.
-			Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
-			Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
-
-			// Find the magnitude of the vector (the distance) between the touches in each frame.
-			float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
-			float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
-
-			// Find the difference in the distances between each frame.
-			float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
-
-			// Change image scale
-			scaling = imageTransform.localScale.x - (deltaMagnitudeDiff * zoomSpeed * Time.deltaTime * Screen.currentResolution.height);
-
-			// code for maximum and minimum zoom limit
-			scaling = Mathf.Max(scaling, 1.0f);
-			scaling = Mathf.Min(scaling, 4.5f);
-
-			//implement the scale value to image
-			currentScale.x = scaling;
-			currentScale.y = scaling;
-			imageTransform.localScale = currentScale; 
-
-			if (deltaMagnitudeDiff > 0.0f) { //zoom out back to center
-				imageTransform.localPosition = Vector2.MoveTowards(imageTransform.localPosition, 
-					Vector2.zero, 
-					backToCenterSpeed * Time.deltaTime * deltaMagnitudeDiff * scaling * Screen.currentResolution.height);
-			}
-
-			//set x and y limit depend on scale
-			limitX = defaultlimitX * (scaling-1);
-			limitY = defaultlimitY * (scaling-1);
-		}
-
-
-		if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)       //Single Touch Began
-		{
-			//set fill delay parameter
-			nextWait = Time.time + holdRate;
-
-			//store the first touch position
-			Touch touch = Input.GetTouch(0);
-			firstPos.x = touch.position.x;
-			firstPos.y = touch.position.y;
-		}
-
-		if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved) //single touch move or hold     //Single Touch Moved
-		{
-
-			if (scaling > 1.01f)
+			if (Input.touchCount == 2)      //For Detecting Multiple Touch On Screen
 			{
-				//store the next touch position
-				Touch touch = Input.GetTouch(0);
-				Vector2 secPos;
-				secPos.x = touch.position.x;
-				secPos.y = touch.position.y;
+				if (Input.GetTouch(0).phase == TouchPhase.Began) {
+					//set fill delay parameter
+					nextWait = Time.time + holdRate;
+				}
 
-				//calculate panning value
-				deltaPosX = (secPos.x - firstPos.x)*Time.deltaTime*panSpeed* Screen.currentResolution.height;
-				deltaPosY = (secPos.y - firstPos.y)*Time.deltaTime*panSpeed* Screen.currentResolution.height;
 
-				//add panning value to current position
-				currentPosition.x = imageTransform.localPosition.x + deltaPosX;
-				currentPosition.y = imageTransform.localPosition.y + deltaPosY;
+				// Store both touches.
+				Touch touchZero = Input.GetTouch(0);
+				Touch touchOne = Input.GetTouch(1);
 
-				//limit panning
+
+				// Find the position in the previous frame of each touch.
+				Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+				Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+				// Find the magnitude of the vector (the distance) between the touches in each frame.
+				float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+				float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+				// Find the difference in the distances between each frame.
+				float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+				// Change image scale
+				scaling = imageTransform.localScale.x - (deltaMagnitudeDiff * ScreenScale.x * ScreenScale.y * zoomSpeed * Time.deltaTime);
+
+				// code for maximum and minimum zoom limit
+				scaling = Mathf.Max(scaling, 1.0f);
+				scaling = Mathf.Min(scaling, 6f);
+
+				//implement the scale value to image
+				currentScale.x = scaling;
+				currentScale.y = scaling;
+				imageTransform.localScale = currentScale; 
+
+				//set x and y limit depend on scale
+				limitX = defaultlimitX * (scaling-1);
+				limitY = defaultlimitY * (scaling-1);
+
+				//get image position
+				currentPosition.x = imageTransform.localPosition.x;
+				currentPosition.y = imageTransform.localPosition.y;
+
+				//limit image position
 				currentPosition.x = Mathf.Min (currentPosition.x, limitX);
 				currentPosition.x = Mathf.Max (currentPosition.x, -limitX);
 
 				currentPosition.y = Mathf.Min (currentPosition.y, limitY);
 				currentPosition.y = Mathf.Max (currentPosition.y, -limitY);
 
-				//implement panning position
+				//implement limit position
 				imageTransform.localPosition = currentPosition;
 			}
+
+
+			if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)       //Single Touch Began
+			{
+				//set fill delay parameter
+				nextWait = Time.time + holdRate;
+
+				//store the first touch position
+				Touch touch = Input.GetTouch(0);
+				firstPos.x = touch.position.x;
+				firstPos.y = touch.position.y;
+			}
+
+			if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved) //single touch move or hold     //Single Touch Moved
+			{
+
+				if (scaling > 1.01f)
+				{
+					//store the next touch position
+					Touch touch = Input.GetTouch(0);
+					Vector2 secPos;
+					secPos.x = touch.position.x;
+					secPos.y = touch.position.y;
+
+					//calculate range from first touch, normalize by screen resolution
+					deltaPosX = (secPos.x - firstPos.x) * ScreenScale.x;
+					deltaPosY = (secPos.y - firstPos.y) * ScreenScale.y;
+
+					if (Mathf.Abs(deltaPosX)>nudgeRange && Mathf.Abs(deltaPosY)>nudgeRange) { //Avoid moving while finger only nudge a little
+						//calculate panning value
+						deltaPosX = deltaPosX*Time.deltaTime*panSpeed;
+						deltaPosY = deltaPosY*Time.deltaTime*panSpeed;
+
+						//add panning value to current position
+						currentPosition.x = imageTransform.localPosition.x + deltaPosX;
+						currentPosition.y = imageTransform.localPosition.y + deltaPosY;
+
+						//limit panning
+						currentPosition.x = Mathf.Min (currentPosition.x, limitX);
+						currentPosition.x = Mathf.Max (currentPosition.x, -limitX);
+
+						currentPosition.y = Mathf.Min (currentPosition.y, limitY);
+						currentPosition.y = Mathf.Max (currentPosition.y, -limitY);
+
+						//implement panning position
+						imageTransform.localPosition = currentPosition;
+
+						//refresh first position
+						firstPos.x = secPos.x;
+						firstPos.y = secPos.y;
+					}
+
+				}
+			}
+
+			if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Stationary) {
+				//store the first touch position
+				Touch touch = Input.GetTouch(0);
+				firstPos.x = touch.position.x;
+				firstPos.y = touch.position.y;
+			}
 		}
+
 
 	}
 
 
+	public void coloringScreenTouched(){
+		isTouching = true;
+	}
 
+	public void coloringScreenReleased(){
+		isTouching = false;
+	}
 
 
 
@@ -241,6 +307,8 @@ public class pixelmanipulated : MonoBehaviour {
 	//fill
 	
 	public void DebugPoint(BaseEventData data){
+		
+		coloringScreenReleased ();
 
 		if (Time.time < nextWait) { //fill delay condition
 			
